f74f823108a3acf3c52b3774b2a21405fed47b02
Andres Alejandro Sañudo Covarrubias <asanudo@imca.local>
2020-10-20 15:38:20 -0600

    change state

--- src/component/Formulario.jsx
+++ src/component/Formulario.jsx
@@ -1,7 +1,27 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 
 const Formulario = () => {
+
+     //* Crear state de citas
+     const [cita, actualizarCita] = useState({
+          paciente:'',
+          tutor:'',
+          fecha:'',
+          hora:'',
+          sintomas:''
+
+     })
+
+     //* Function que se ejecuta cuando escribe el usuario
+     const handleChange = e => {
+          actualizarCita({
+               ...cita,
+               [e.target.name] : e.target.value
+          })
+     }
+
+
      return ( 
           <Fragment>
                <h2>Crear Citas:</h2>
@@ -11,8 +31,9 @@ const Formulario = () => {
                     <input 
                          type="text" 
                          placeholder="Nombre Completo"
-                         name="persona"
+                         name="paciente"
                          className="u-full-width"
+                         onChange={handleChange}
                     />
                     <label>Tutor:</label>
                     <input 
@@ -20,31 +41,35 @@ const Formulario = () => {
                          name="tutor"
                          placeholder="Nombre del Tutor"
                          className="u-full-width"
+                         onChange={handleChange}
                     />
                     <label>Fecha:</label>
                     <input 
                          type="date"
                          name="fecha"
                          className="u-full-width"
+                         onChange={handleChange}
                     />
                     <label>Hora:</label>
                     <input 
                          type="time"
                          name="hora"
                          className="u-full-width"
+                         onChange={handleChange}
                     />
                     <label>Sintomas:</label>
                     <textarea 
                          name="sintomas" 
                          className="u-full-width"
                          resize="none"
+                         onChange={handleChange}
                     ></textarea>
                     <button
                          type="submit"
                          className="u-full-width button-primary"
                     >Agendar Cita</button>
                </form>
-               <label> something</label>
+
           </Fragment>
       );
 }
